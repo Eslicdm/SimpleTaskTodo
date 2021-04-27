7879209c2d7021ae8eba72c53cadc1051e79b4ef
Eslicdm <eslicdm1@gmail.com>
2021-04-27 11:07:44 -0300

    adding a list in ExtendedListFragment.kt

--- app/src/main/java/com/eslirodrigues/simpletasktodo/ui/ExtendedListFragment.kt
+++ app/src/main/java/com/eslirodrigues/simpletasktodo/ui/ExtendedListFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.eslirodrigues.simpletasktodo.adapter.TodoAdapter
 import com.eslirodrigues.simpletasktodo.databinding.FragmentExtendedListBinding
+import com.eslirodrigues.simpletasktodo.model.Todo
 
 class ExtendedListFragment : Fragment(){
 
@@ -14,6 +17,7 @@ class ExtendedListFragment : Fragment(){
     private val binding get() = _binding!!
 
     private val args: ExtendedListFragmentArgs by navArgs()
+    private lateinit var todoAdapter: TodoAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,7 +30,32 @@ class ExtendedListFragment : Fragment(){
 
         binding.textViewItemTitle.text = title
 
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        binding.recyclerViewExtendedList.adapter = todoAdapter
+        binding.recyclerViewExtendedList.layoutManager = LinearLayoutManager(context)
+
+        addItemClickListener()
+        deleteItemClickListener()
+
         return binding.root
+    }
+
+    private fun addItemClickListener() {
+        binding.buttonAddExtended.setOnClickListener {
+            val inputText = binding.editTextTaskExtended.text.toString()
+            if (inputText.isNotEmpty()) {
+                val task = Todo(inputText)
+                todoAdapter.addTodo(task)
+                binding.editTextTaskExtended.text.clear()
+            }
+        }
+    }
+
+    private fun deleteItemClickListener() {
+        binding.buttonDeleteExtended.setOnClickListener {
+            todoAdapter.deleteDoneTodo()
+        }
     }
 
     override fun onDestroyView() {
