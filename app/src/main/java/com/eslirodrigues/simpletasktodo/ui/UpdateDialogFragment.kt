ac0443ec42c3cf3825c8d28b7c463214980715e5
Eslicdm <eslicdm1@gmail.com>
2021-06-07 07:45:47 -0300

    toast for update task

--- app/src/main/java/com/eslirodrigues/simpletasktodo/ui/UpdateDialogFragment.kt
+++ app/src/main/java/com/eslirodrigues/simpletasktodo/ui/UpdateDialogFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -45,6 +46,7 @@ class UpdateDialogFragment : DialogFragment() {
                 )
             )
             findNavController().navigate(R.id.action_updateDialogFragment_to_listFragment)
+            Toast.makeText(requireContext(), R.string.updated, Toast.LENGTH_SHORT).show()
         }
 
         binding.buttonCancelUpdate.setOnClickListener {
