6b759c5b74fdbdba1c5bd7cdbb869c06122d49fb
Eslicdm <eslicdm1@gmail.com>
2021-07-27 09:54:01 -0300

    adjusting keyboard focus and keyboard action done in sign in screen

--- app/src/main/java/com/eslirodrigues/simpletasktodo/ui/login/SignInFragment.kt
+++ app/src/main/java/com/eslirodrigues/simpletasktodo/ui/login/SignInFragment.kt
@@ -6,6 +6,7 @@ import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -26,7 +27,14 @@ class SignInFragment : Fragment() {
         _binding = FragmentSignInBinding.inflate(inflater, container, false)
 
         openForgotPassword()
+        closeActivityOnBackPressed()
+        openSignUp()
+        logIn()
 
+        return binding.root
+    }
+
+    private fun closeActivityOnBackPressed() {
         binding.root.isFocusableInTouchMode = true
         binding.root.requestFocus()
         binding.root.setOnKeyListener { _, keyCode, _ ->
@@ -37,62 +45,80 @@ class SignInFragment : Fragment() {
                 false
             }
         }
+        binding.root.isFocusableInTouchMode = false
+    }
 
+    private fun openSignUp() {
         binding.textViewSignUp.setOnClickListener {
             findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
         }
-
-        binding.buttonLogin.setOnClickListener {
-            when {
-                TextUtils.isEmpty(binding.textInputEditTextEmail.text.toString().trim { it <= ' ' }) -> {
-                    Toast.makeText(
-                        requireContext(),
-                        "Please Enter Email",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-
-                TextUtils.isEmpty(binding.textInputEditTextPassword.text.toString().trim { it <= ' ' }) -> {
-                    Toast.makeText(
-                        requireContext(),
-                        "Please Enter Password",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-                else -> {
-                    val email: String = binding.textInputEditTextEmail.text.toString().trim { it <= ' ' }
-                    val password: String = binding.textInputEditTextPassword.text.toString().trim { it <= ' ' }
-
-                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener { task ->
-                            if (task.isSuccessful) {
-//                                val firebaseUser: FirebaseUser = task.result!!.user!!
-                                Toast.makeText(
-                                    requireContext(),
-                                    "You were logged in successfully",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-
-                                findNavController().navigate(R.id.action_signInFragment_to_listFragment)
-
-                            } else {
-                                Toast.makeText(
-                                    requireContext(),
-                                    task.exception!!.message.toString(),
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                        }
-                }
-            }
-        }
-
-        return binding.root
     }
 
     private fun openForgotPassword() {
         binding.textViewForgotPassword.setOnClickListener {
             findNavController().navigate(R.id.action_signInFragment_to_forgotPasswordFragment)
+        }
+    }
+
+    private fun baseLogin() {
+        when {
+            TextUtils.isEmpty(
+                binding.textInputEditTextEmail.text.toString().trim { it <= ' ' }) -> {
+                Toast.makeText(
+                    requireContext(),
+                    "Please Enter Email",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            TextUtils.isEmpty(
+                binding.textInputEditTextPassword.text.toString()
+                    .trim { it <= ' ' }) -> {
+                Toast.makeText(
+                    requireContext(),
+                    "Please Enter Password",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            else -> {
+                val email: String =
+                    binding.textInputEditTextEmail.text.toString().trim { it <= ' ' }
+                val password: String =
+                    binding.textInputEditTextPassword.text.toString().trim { it <= ' ' }
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+//                                val firebaseUser: FirebaseUser = task.result!!.user!!
+                            Toast.makeText(
+                                requireContext(),
+                                "You were logged in successfully",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            findNavController().navigate(R.id.action_signInFragment_to_listFragment)
+
+                        } else {
+                            Toast.makeText(
+                                requireContext(),
+                                task.exception!!.message.toString(),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun logIn() {
+        binding.textInputEditTextPassword.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                baseLogin()
+            }
+            return@setOnEditorActionListener true
+        }
+        binding.buttonLogin.setOnClickListener {
+            baseLogin()
         }
     }
 
